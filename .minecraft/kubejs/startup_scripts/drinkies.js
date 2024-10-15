6461810d28f1ce5ca60e1c5af20e8f8e497e6613
fmbellomy <fmbellomy@quantumgarbage.com>
2024-10-15 13:45:47 -0500

    excessive comments so that my friends can comprehend the jank

--- .minecraft/kubejs/startup_scripts/drinkies.js
+++ .minecraft/kubejs/startup_scripts/drinkies.js
@@ -32,26 +32,37 @@ StartupEvents.registry("item", (event) => {
   event.create("silly_straw").texture("kubejs:item/silly_straw");
 });
 
+// a mapping between the item id of a given drinky to the function that should be run when it's consumed
 let effectsMap = {
   "minecraft:manure_drinky": function (itemstack, entity) {
     entity.kill();
     return itemstack;
   },
-
+  // default function to run if the drinky does not have a unique effect
   default: function (itemstack, entity) {
     let effects = entity.potionEffects;
     effects.add("minecraft:blindness", 10 * 20);
     return itemstack;
   },
 };
-
+// function that is run whenever you consume a drinky
+// this is global so that way hot-reloading works
 global.consumeDrinky = (itemstack, entity) => {
+  // only run this if the consuming entity is a player
+  // (i dont know how it would ever be anything else,
+  // but since we're using player-exclusive functions
+  // it would crash the game if that happened)
   if (entity.player) {
+    // remove one drinky
     itemstack.shrink(1);
+    // give back an empty bucket
     entity.addItem(Item.of("minecraft:bucket"));
+    // if the effects map actually has a function for this particular drinky
     if (effectsMap[itemstack.item]) {
+      // run the function associated with this drinky
       return effectsMap[itemstack.item](itemstack, entity);
     } else {
+      // otherwise run the default function
       return effectsMap["default"](itemstack, entity);
     }
   }
